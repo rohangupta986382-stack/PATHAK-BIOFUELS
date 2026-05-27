dfba28bc4b91ce24bfd4b569daf24f19aa35105b
rohangupta986382-stack <rohangupta986382@gmail.com>
2026-05-27 14:50:48 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+function toggleSection(id) {
+  const sections = ['quote', 'contact', 'products', 'about'];
+
+  sections.forEach(section => {
+    if(section === id){
+      document.getElementById(section).classList.toggle('hidden');
+    } else {
+      document.getElementById(section).classList.add('hidden');
+    }
+  });
+}
